7501d2b15e454e42e9656408fdce9126483f84ea
DkChriss <chrisshuayhua@hotmail.com>
2025-05-12 15:40:04 -0400

    Delete comment

--- services/security/utils/dependency.py
+++ services/security/utils/dependency.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import Session
 from services.security.config.database import SessionLocal
 
-# Dependency to get the database session
 def get_db() -> Session:
     db = SessionLocal()
     try:
