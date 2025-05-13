1c9734185286eee3cde6e1ebc32cbf624ec30e46
DkChriss <chrisshuayhua@hotmail.com>
2025-05-13 15:55:53 -0400

    Add routes roles

--- services/security/main.py
+++ services/security/main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
 from starlette.middleware.cors import CORSMiddleware
 from services.security.config.database import Base, engine
+from fastapi_pagination import add_pagination
+#ROUTES
 from services.security.controllers.user import router as user_router
 from services.security.controllers.auth import router as auth_router
-from fastapi_pagination import add_pagination
-
-#IMPORT MODELS FOR CREATING TABLES
+from services.security.controllers.role import router as role_router
+#MODELS
 from services.security.models.permission import Permission
 from services.security.models.user import User
 from services.security.models.role import Role
-from services.security.models.association import *
+from services.security.models.user_role import UserRole
+from services.security.models.role_permission import RolePermission
+from services.security.models.user_permission import UserPermission
 app = FastAPI()
 
 origins = [
@@ -27,6 +31,9 @@ app.add_middleware(
 # CREATE TABLES
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
+
+#ROUTES
 app.include_router(user_router, prefix="/api/v1", tags=["users"])
 app.include_router(auth_router, prefix="/api/v1", tags=["auth"])
+app.include_router(role_router, prefix="/api/v1", tags=["roles"])
 add_pagination(app)
